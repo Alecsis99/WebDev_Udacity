7f0aa3c4b33b49a3108c814965f97a5bd8bc0b18
Florin <florin.urian@ca.fujitsu.com>
2015-03-04 15:40:24 -0500

    - 'Really'

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,11 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello Udacity! This is cool!')
-        self.response.write('Hello Udacity! This is cool!')
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Hello Udacity! This is cool!')
+		self.response.write('Hello Udacity! This is really cool!')
+		self.response.write('Hello Udacity! This is really really cool!')
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
