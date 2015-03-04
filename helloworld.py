421f0a4fdb66472546232eb4a7c58a57fbeecade
Florin <florin.urian@ca.fujitsu.com>
2015-03-04 15:06:03 -0500

    Try some changes

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello Udacity! This is cool!')
+        self.response.write('Hello Udacity! This is cool!')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
