10da22529d9925284e12347772e61308ea5e669f
Alecsis99 <alecsis_99@yahoo.com>
2015-03-04 14:19:47 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello Udacity! This is cool!')
-
+        self.response.write('See if the files are updated in Google')
 application = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
